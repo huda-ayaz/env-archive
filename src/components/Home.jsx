837fec985c9777f5ddeb535a1b17b04398ee10ad
Huda Ayaz <hudaayaz21@gmail.com>
2023-10-22 00:27:20 -0400

    Work on terminal and styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,11 +2,12 @@ import React, { useContext, useState, useEffect } from 'react';
 import { Navigate, redirect, useNavigate, Link } from 'react-router-dom';
 import Popup from './Popup';
 import Terminal from './Terminal';
+import "../App.css";
+import "../stylesheets/Home.css";
 
 const Home = () => {
     return(
         <div>
-            <h1>Home</h1>
             <Terminal/>
             <Popup/>
         </div>
